b5fabd70fd74f273227e75744bdec07840eb05a3
Rubanov <izvorg@yandex.ru>
2022-05-01 18:27:47 +0300

    Попытался сделать каталог

---
+++ CRMSystem/View/CustomerViews/CatalogFrame.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using System.Windows.Controls;
+
+namespace CRMSystem.View.CustomerViews
+{
+    public partial class CatalogFrame : Page
+    {
+        public CatalogFrame()
+        {
+            InitializeComponent();
+            CRMSystemEntities DB = new CRMSystemEntities();
+            listing.ItemsSource = DB.Products.ToList();
+        }
+    }
+}
--- CRMSystem/View/CustomerViews/CustomerWindow.xaml.cs
+++ CRMSystem/View/CustomerViews/CustomerWindow.xaml.cs
@@ -4,9 +4,20 @@ namespace CRMSystem.View.CustomerViews
 {
     public partial class CustomerWindow : Window
     {
+
         public CustomerWindow(Users customer)
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new CatalogFrame());
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
