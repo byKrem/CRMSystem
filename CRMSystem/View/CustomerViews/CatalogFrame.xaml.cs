1637ba84ab37c7388bf65df01c54bd18d808a90a
Rubanov <izvorg@yandex.ru>
2022-05-04 07:21:21 +0300

    Реализовал каталог товаров для Клиента

--- CRMSystem/View/CustomerViews/CatalogFrame.xaml.cs
+++ CRMSystem/View/CustomerViews/CatalogFrame.xaml.cs
@@ -1,15 +1,32 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Windows.Controls;
 
 namespace CRMSystem.View.CustomerViews
 {
     public partial class CatalogFrame : Page
     {
-        public CatalogFrame()
+        private CustomerWindow _customerWindow;
+        private DateTime delta;
+        public CatalogFrame(CustomerWindow customerWindow)
         {
             InitializeComponent();
+            _customerWindow = customerWindow;
+            CartIndicatorBlock.Text = $"В корзине: {_customerWindow.Cart.Count} шт.";
             CRMSystemEntities DB = new CRMSystemEntities();
-            listing.ItemsSource = DB.Products.ToList();
+            ListViewProducts.ItemsSource = DB.Products.ToList();
+        }
+
+        private void Border_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if(DateTime.Now - delta < TimeSpan.FromMilliseconds(500))
+            {
+                Products product = (sender as Border).DataContext as Products;
+                _customerWindow.Cart.Add(product);
+                CartIndicatorBlock.Text = $"В корзине: {_customerWindow.Cart.Count} шт.";
+            }
+
+            delta = DateTime.Now;
         }
     }
 }
--- CRMSystem/View/CustomerViews/CustomerWindow.xaml.cs
+++ CRMSystem/View/CustomerViews/CustomerWindow.xaml.cs
@@ -1,23 +1,28 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
 
 namespace CRMSystem.View.CustomerViews
 {
     public partial class CustomerWindow : Window
     {
-
+        public List<Products> Cart { get; private set; }
+        private Users _currentCustomer;
         public CustomerWindow(Users customer)
         {
             InitializeComponent();
+            Cart = new List<Products>();
+            _currentCustomer = customer;
+            MainFrame.Navigate(new CatalogFrame(this));
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            MainFrame.Navigate(new CatalogFrame());
+            MainFrame.Navigate(new CatalogFrame(this));
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-
+            MainFrame.Navigate(new CartFrame(Cart));
         }
     }
 }
